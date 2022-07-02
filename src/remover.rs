109ae51e7c0b6f031d87467e3fa9287b8ef4bafd
Shoshak <61428316+Shoshak@users.noreply.github.com>
2022-07-03 00:52:21 +0500

    fixed file detection in remover

--- src/remover.rs
+++ src/remover.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 pub fn remove_modules(gmod_installation_directory: &Path) {
     for f in constants::GARRYSMOD_MODULES {
-        let full_path = gmod_installation_directory.join("garrysmod").join(f);
+        let full_path = gmod_installation_directory.join(f);
         if full_path.exists() {
             std::fs::remove_file(&full_path)
                 .expect(&format!("Could not remove file at {}", full_path.display()));
